8a6d3c5c7098e5dff24f8e1cfd3e324e346a724b
Martí Bosch <marti.bosch@epfl.ch>
2018-10-12 10:48:43 +0200

    added yapf configuration, basic class and io methods

--- sfso_geopy/__init__.py
+++ sfso_geopy/__init__.py
@@ -6,43 +6,61 @@ import rasterio
 
 from rasterio.transform import from_origin
 
+DEFAULT_INDEX_COLUMN = 'RELI'
+DEFAULT_CRS = {'init': 'epsg:21781'}
+DEFAULT_RES = (100, 100)
+
 
 class SFSOGeoDataFrame(pd.DataFrame):
     """Documentation for SFSOGeoDataFrame
 
     """
 
-    def __init__(self, xres=100, yres=100, crs={'init': 'epsg:21781'}):
-        super(SFSOGeoDataFrame, self).__init__()
-        self.xres = xres
-        self.yres = yres
-        self.crs = crs
+    # so that pandas can allow setting this class attributes
+    _metadata = ['crs', 'res']
 
-    def to_geotiff(self, filename, col_name, nodata=0, dtype=rasterio.uint8):
+    def __init__(self, *args, **kwargs):
+        crs = kwargs.pop('crs', DEFAULT_CRS)
+        res = kwargs.pop('res', DEFAULT_RES)
+        super(SFSOGeoDataFrame, self).__init__(*args, **kwargs)
+        self.set_index(DEFAULT_INDEX_COLUMN, inplace=True)
+        self.crs = crs
+        self.res = res
+
+    def to_ndarray(self, column, nodata=0, dtype=np.uint8):
         x = self['X'].values
         y = self['Y'].values
-        z = self[col_name].values
+        z = self[column].values
 
-        i = (y - min(y)) // self.yres
-        j = (x - min(x)) // self.xres
+        xres, yres = self.res
+        i = (y - min(y)) // yres
+        j = (x - min(x)) // xres
 
-        z_arr = np.nan * np.empty((len(set(i)), len(set(j))))
-        z_arr[-i, j] = z
-        x_origin = min(x) - self.xres // 2
-        y_origin = max(y) + self.yres // 2
-        self.transform = from_origin(x_origin, y_origin, self.xres, self.yres)
+        lulc_arr = np.nan * np.empty((len(set(i)), len(set(j))))
+        lulc_arr[-i, j] = z
+        x_origin = min(x) - xres // 2
+        y_origin = max(y) + yres // 2
+        self.transform = from_origin(x_origin, y_origin, xres, yres)
 
-        z_arr[np.isnan(z_arr)] = nodata
+        lulc_arr[np.isnan(lulc_arr)] = nodata
 
-        with rasterio.open(
-                filename,
-                'w',
-                driver='GTiff',
-                height=z_arr.shape[0],
-                width=z_arr.shape[1],
-                count=1,
-                dtype=str(dtype),
-                nodata=0,
-                crs=self.crs,
-                transform=self.transform) as raster:
-            raster.write(z_arr.astype(dtype), 1)
+        return lulc_arr.astype(dtype)
+
+    def to_geotiff(self, fp, column, nodata=0, dtype=rasterio.uint8):
+        lulc_arr = self.to_ndarray(column, nodata, dtype)
+
+        with rasterio.open(fp, 'w', driver='GTiff', height=lulc_arr.shape[0],
+                           width=lulc_arr.shape[1], count=1, dtype=str(dtype),
+                           nodata=0, crs=self.crs,
+                           transform=self.transform) as raster:
+
+            raster.write(lulc_arr.astype(dtype), 1)
+
+
+def read_csv(filepath_or_buffer, crs=None, res=None, *args, **kwargs):
+    if crs:
+        kwargs['crs'] = crs
+    if res:
+        kwargs['res'] = res
+    df = pd.read_csv(filepath_or_buffer, *args, **kwargs)
+    return SFSOGeoDataFrame(df, *args, **kwargs)
