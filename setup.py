bd7a69ebbb9558b70371db2ee024b7bd080fcbbd
Martí Bosch <marti.bosch@epfl.ch>
2019-05-17 14:25:19 +0200

    Bump version: 0.5.1 → 0.6.0

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def setup_package():
     # yapf: disable
     setup(
         name='swisslandstats-geopy',
-        version='0.5.1',
+        version='0.6.0',
         description='Python for the land statistics datasets from the SFSO',
         long_description=long_description,
         long_description_content_type='text/markdown',
